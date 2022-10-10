3dc634536eb4300a81600c6ed70bd2d185890a48
asassaki <alinesassaki@hotmail.com>
2022-10-10 12:17:12 -0300

    Adiciona @Param na CategoriaRepository

--- src/main/java/com/generation/colheitafresca/repository/CategoriaRepository.java
+++ src/main/java/com/generation/colheitafresca/repository/CategoriaRepository.java
@@ -3,6 +3,7 @@ package com.generation.colheitafresca.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.colheitafresca.model.Categoria;
@@ -10,6 +11,6 @@ import com.generation.colheitafresca.model.Categoria;
 @Repository
 public interface CategoriaRepository extends JpaRepository <Categoria, Long>{
 	
-	public List<Categoria> findAllByTituloContainingIgnoreCase(String titulo);
+	public List<Categoria> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
